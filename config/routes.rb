ca9f24cf7ba32c27484cc9292af50eb484af1323
Nate Jackson <nate@homeplanbiz.com>
2016-07-05 09:07:24 +0000

    updated routes.rb moved devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   
-  devise_for :users, controllers: {registrations: 'users/registrations' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  devise_for :users, controllers: {registrations: 'users/registrations' }
   resources :contacts
   get '/about' => 'pages#about'
   root 'pages#home'
