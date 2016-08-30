68a2d9d2e05c90938df062e4c3421890d8ed7b4a
Nate Jackson <nate@homeplanbiz.com>
2016-08-30 14:37:18 +0000

    Added profiles new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,8 @@
 class ProfilesController < ApplicationController
-    
+    def new
+        # form for each user to create their own profile
+        @user = User.find( params[:user_id])
+        @variable = params[:hello]
+        @profile = @user.build_profile
+    end
 end
